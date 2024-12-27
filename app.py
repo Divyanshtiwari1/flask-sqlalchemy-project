814a0e89b0e808ba35492491c9558310697f337c
Divyansh Tiwari <153393147+Divyanshtiwari1@users.noreply.github.com>
2024-12-27 15:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+import os
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
-
+if _name_ == "_main_":
+    port = 5000  
+    if "PORT" in os.environ:
+        port = int(os.environ["PORT"])
+    
+   
+    app.run(debug=False, host="0.0.0.0", port=port)
 
